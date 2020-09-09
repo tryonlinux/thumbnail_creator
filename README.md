c65b09a2ccf85c65cad61c6c11ac9af4af718887
Jordan Tryon <tryonlinux@gmail.com>
2020-09-08 22:29:31 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # thumbnail_creator
 Creates thumbnails for every image in a folder
+To launch, pass in a folder name like below: 
+
+``` node index.js '/home/jordan/pics' ``` 
---
+++ index.js
@@ -0,0 +1,32 @@
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
+var filepath;
+const convertToThumbnail = async function(filepath,filename){
+  
+    var safeFileName = path.normalize(filename).replace(/[^a-z0-9]/gi, "-");
+    await sharp(path.join(filepath,filename))
+    .resize(640, 427, {
+      fit: sharp.fit.inside,
+      withoutEnlargement: true
+    })
+    .png()
+    .toFile(
+      path.join(
+        filepath,
+        "thumb.quickview." + safeFileName + ".png"
+      )
+    );
+}
+
+const getAllFiles = async function(filepath){
+    return  await fs.readdirSync(filepath);
+}
+
+filepath = process.argv[2];
+getAllFiles(filepath).then(files =>{
+    files.forEach(filename => {
+        convertToThumbnail(filepath,filename)
+    });
+})
